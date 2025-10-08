1f2055319788d51c2e8cf1aad5fcb75538c8c98f
Daniel <danieldsm.on@gmail.com>
2025-10-07 23:39:40 -0300

    implement write and read functions for steganography

--- main.py
+++ main.py
@@ -32,24 +32,38 @@ def gamma(img: np.ndarray, y: np.double, c: np.double = 1) -> np.ndarray:
 def bit_slice(img: np.ndarray, k: np.uint8):
     return img & (2**(k-1))
 
-def steganography(img: np.ndarray, msg: str):
+def write_steganography(img: np.ndarray, msg: str) -> np.ndarray:
     new = img.copy()
-    index = 0
+    msg_index = 0
     shift = 7
     msg_length = len(msg)
-    # usando iterador para acessar indices
-    it = np.nditer(img, flags=['multi_index'])
-    for pixel in it:
+    for index, pixel in np.ndenumerate(img):
         if (shift < 0):
             shift = 7
-            index += 1
-        if(index < msg_length):
-            char = ord(msg[index])
+            msg_index += 1
+        if (msg_index < msg_length):
+            char = ord(msg[msg_index])
             bit = (char >> shift) & 0b00000001
-            pixel = (pixel & 0b11111110) | bit # definindo ultimo bit do pixel
-            new[it.multi_index] = pixel # salvando pixel alterado na nova imagem
+            new[index] = (pixel & 0b11111110) | bit # definindo ultimo bit do pixel
             shift -= 1
+        else:
+            new[index] = pixel & 0b11111110 # restante dos pixels vai zerar o ultimo bit
     return new
+
+def read_steganography(img: np.ndarray) -> str:
+    msg = []
+    char = 0
+    bits = 0
+    for pixel in np.nditer(img):
+        bit = pixel & 0b00000001
+        char = (char << 1) | bit
+        bits += 1
+        if (bits == 8):
+            if (char != 0):
+                msg.append(chr(char))
+            bits = 0
+            char = 0
+    return "".join(msg) # transformando lista de caracteres em uma string
 
 def main():
     img = cv.imread("paisagem.jpg", cv.IMREAD_GRAYSCALE)
@@ -57,10 +71,6 @@ def main():
     if (img is None):
         print("Could not read the image!")
         return 0
-
-    new = steganography(img, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-
-    print(np.sum(img - new))
 
     return 0
 
