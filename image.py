a77a45c218b88312bfad1a67f6f0336dbda3f5c6
Daniel <danieldsm.on@gmail.com>
2025-10-07 23:33:56 -0300

    implement log and gamma functions

--- image.py
+++ image.py
@@ -4,35 +4,40 @@ import numpy as np
 def convert(img: np.ndarray) -> np.ndarray:
     return img.astype(np.double) / 255
 
+def deconvert(img: np.ndarray) -> np.ndarray:
+    return (img * 255).astype(np.uint8)
+
 def brightness(img: np.ndarray, k: np.double = 0.5) -> np.ndarray:
     return (img * k)
 
-def negative(img: np.ndarray):
+def negative(img: np.ndarray) -> np.ndarray:
     return 1 - img
 
 def threshold(img: np.ndarray, k: np.double = 0.5) -> np.ndarray:
     return (img > k) * 1.0 # transform boolean into double
 
-def constrast(img: np.ndarray):
+def constrast(img: np.ndarray) -> np.ndarray:
     min = np.min(img)
     tmp = img - min
     max = np.max(tmp)
     tmp = tmp / max
     return tmp
 
-img = cv.imread("low2.jpg", cv.IMREAD_GRAYSCALE)
+def log(img: np.ndarray, c: np.double = 1) -> np.ndarray:
+    return c * np.log2(1 + img)
 
-if img is None:
-    print("Could not read the image!")
+def gamma(img: np.ndarray, y: np.double, c: np.double = 1) -> np.ndarray:
+    return c * np.pow(img, y) 
 
-img = convert(img)
+def main():
+    img = cv.imread("paisagem.jpg", cv.IMREAD_GRAYSCALE)
 
-print(np.min(img))
-print(np.max(img))
-print(np.min(constrast(img)))
-print(np.max(constrast(img)))
+    if img is None:
+        print("Could not read the image!")
+        return 0
 
-cv.imshow("Image", img)
-cv.waitKey(0)
-cv.imshow("Image", constrast(img))
-cv.waitKey(0)
+    img = convert(img)
+
+    return 0
+
+main()
