1e412fef0a06150f391373a5a29d22c23d49aafc
Daniel <danieldsm.on@gmail.com>
2025-10-07 23:29:27 -0300

    implement some processing functions

---
+++ image.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+
+def convert(img: np.ndarray) -> np.ndarray:
+    return img.astype(np.double) / 255
+
+def brightness(img: np.ndarray, k: np.double = 0.5) -> np.ndarray:
+    return (img * k)
+
+def negative(img: np.ndarray):
+    return 1 - img
+
+def threshold(img: np.ndarray, k: np.double = 0.5) -> np.ndarray:
+    return (img > k) * 1.0 # transform boolean into double
+
+img = cv.imread("paisagem.jpg", cv.IMREAD_GRAYSCALE)
+
+if img is None:
+    print("Could not read the image!")
+
+img = convert(img)
+
+print(negative(img))
+
+cv.imshow("Image", img)
+cv.waitKey(0)
