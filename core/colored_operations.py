17ace8dacb5f99113a793384b81262c879e21b9e
Daniel <danieldsm.on@gmail.com>
2025-10-28 16:49:48 -0300

    create color adjustments

--- core/colored_operations.py
+++ core/colored_operations.py
@@ -48,3 +48,42 @@ def colored_histogram_equalization(img: np.ndarray) -> np.ndarray:
 
     # retornando a imagem equalizada
     return new
+
+def adjust_hsi(hsi: np.ndarray, h: float, s: float, i: float) -> np.ndarray:
+    # ajustando o matiz
+    hsi[..., 0] = (hsi[..., 0] * h) % 360
+
+    # ajustando a saturacao
+    hsi[..., 1] = np.clip(hsi[..., 1] * s, 0, 1)
+
+    # ajustando a intensidade
+    hsi[..., 2] = np.clip(hsi[..., 2] * i, 0, 1)
+
+    # retornando a imagem ajustada
+    return hsi
+
+def adjust_hsv(hsv: np.ndarray, h: float, s: float, v: float) -> np.ndarray:
+    # ajustando o matiz
+    hsv[..., 0] = (hsv[..., 0] * h) % 360
+
+    # ajustando a saturacao
+    hsv[..., 1] = np.clip(hsv[..., 1] * s, 0, 1)
+
+    # ajustando o valor
+    hsv[..., 2] = np.clip(hsv[..., 2] * v, 0, 1)
+
+    # retornando a imagem ajustada
+    return hsv
+
+def adjust_rgb(rgb: np.ndarray, r: float, g: float, b: float) -> np.ndarray:
+    # ajustando o canal vermelho
+    rgb[..., 0] = np.clip(rgb[..., 0] * r, 0, 1)
+
+    # ajustando o canal verde
+    rgb[..., 1] = np.clip(rgb[..., 1] * g, 0, 1)
+
+    # ajustando o canal azul
+    rgb[..., 2] = np.clip(rgb[..., 2] * b, 0, 1)
+
+    # retornando a imagem ajustada
+    return rgb
